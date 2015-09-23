73433db695ca812e9b04fac19db8be65d151de13
DataSciBurgoon <burgoon.lyle@gmail.com>
2015-09-23 13:44:35 -0400

    Mod'd TestParser.scala to use ScalaTest instead of JUnit.

--- build.sbt
+++ build.sbt
@@ -1,10 +1,12 @@
 val json4sNative = "org.json4s" %% "json4s-native" % "3.2.11"
+val scalaTest = "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
 
 lazy val root = (project in file(".")).
 	settings(
 		name := "toxcast raw data parser",
 		version := "1.0",
 		scalaVersion := "2.11.6",
-		libraryDependencies += json4sNative
+		libraryDependencies += json4sNative,
+		libraryDependencies += scalaTest
 	)
 
--- src/test/scala/TestParser.scala
+++ src/test/scala/TestParser.scala
@@ -1,31 +1,91 @@
 package test
 
-import org.junit.Test
-import org.junit.Assert._
+import org.scalatest._
 import ToxCast.ToxcastParser
 
 
 /**
  * @author burgoonl
+
  */
-class TestParser {
-  @Test def testParseFirstDataRow{
-    val file_path = "toxcast_raw_example.txt"
+
+class TestParser extends FlatSpec with ShouldMatchers{
+  
+  "A ToxcastParser" should "create a ToxCast object with all of the pertinent data" in {
+    val file_path = "src/test/resources/toxcast_raw_example.txt"
     val toxcast_parser = new ToxcastParser(file_path)
     val toxcast_data = toxcast_parser.parse()
     
-    assertEquals(toxcast_data(0).chemical_name, "Corticosterone")
-    assertEquals(toxcast_data(0).casrn, "50-22-6")
-    assertEquals(toxcast_data(0).gene_name, "estrogen receptor 1")
-    assertEquals(toxcast_data(0).assay_name, "ACEA_T47D")
-    assertEquals(toxcast_data(0).assay_format_type, "cell-based")
-    assertEquals(toxcast_data(0).assay_source_name, "ACEA")
-    assertEquals(toxcast_data(0).organism, "human")
-    assertEquals(toxcast_data(0).log_concentration, -2.2218, 0.01)
-    assertEquals(toxcast_data(0).response_value, 5.85, 0.01)
+    assertResult("Corticosterone"){
+      toxcast_data(0).chemical_name
+    }
+    
+    assertResult("50-22-6"){
+      toxcast_data(0).casrn
+    }
+    
+    assertResult("estrogen receptor 1"){
+      toxcast_data(0).gene_name
+    }
+    
+    assertResult("ACEA_T47D"){
+      toxcast_data(0).assay_name
+    }
+    
+    
+    assertResult("cell-based"){
+      toxcast_data(0).assay_format_type
+    }
+    
+    assertResult("ACEA"){
+      toxcast_data(0).assay_source_name
+    }
+    
+    assertResult("human"){
+      toxcast_data(0).organism
+    }
+    
+    
+    toxcast_data(0).log_concentration should be ((-2.2218) +- 0.01)
+    toxcast_data(0).response_value should be (5.85 +- 0.01)
+    
+    assertResult("Sodium chlorate"){
+      toxcast_data(18).chemical_name
+    }
+    
+    assertResult("7775-09-9"){
+      toxcast_data(18).casrn
+    }
+    
+    assertResult("nuclear factor of kappa light polypeptide gene enhancer in B-cells 1"){
+      toxcast_data(18).gene_name
+    }
+    
+    assertResult("Tox21_NFkB_BLA_Agonist"){
+      toxcast_data(18).assay_name
+    }
+    
+    
+    assertResult("cell-based"){
+      toxcast_data(18).assay_format_type
+    }
+    
+    assertResult("Tox21"){
+      toxcast_data(18).assay_source_name
+    }
+    
+    assertResult("human"){
+      toxcast_data(18).organism
+    }
+    
+    
+    toxcast_data(18).log_concentration should be (-1.699 +- 0.01)
+    toxcast_data(18).response_value should be (-0.245 +- 0.01)
+    
   }
   
-  @Test def testParseLastDataRow{
+  
+  /*@Test def testParseLastDataRow{
     val file_path = "toxcast_raw_example.txt"
     val toxcast_parser = new ToxcastParser(file_path)
     val toxcast_data = toxcast_parser.parse()
@@ -39,5 +99,5 @@ class TestParser {
     assertEquals(toxcast_data(18).organism, "human")
     assertEquals(toxcast_data(18).log_concentration, -1.699, 0.01)
     assertEquals(toxcast_data(18).response_value, -0.245, 0.01)
-  }
+  }*/
 }
